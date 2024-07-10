746dc849e36b1b761df4ed5fbe3b065adbe11e33
yazdanafra2 <afrayazdan9@gmail.com>
2024-07-10 16:23:30 +0330

    wdnesday latest

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,35 @@
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  Chip,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
+import CancelIcon from "@mui/icons-material/Cancel";
 import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Form = ({ handleSubmit, handleClose }) => {
+  const names = [
+    "Humaira Sims",
+    "Santiago Solis",
+    "Dawid Floyd",
+    "Mateo Barlow",
+    "Samia Navarro",
+    "Kaden Fields",
+    "Genevieve Watkins",
+    "Mariah Hickman",
+    "Rocco Richardson",
+    "Harris Glenn",
+  ];
+
   const [extention, setExtention] = useState("");
   const [mimeType, setMimeType] = useState([]);
+  const [selectedNames, setSelectedNames] = useState([]);
 
   return (
     <>
@@ -28,7 +53,43 @@ const Form = ({ handleSubmit, handleClose }) => {
             setExtention(e.target.value);
           }}
           required
+          sx={{ m: 1, width: 500 }}
         />
+        <FormControl sx={{ m: 1, width: 500 }}>
+          <InputLabel>Mime Types</InputLabel>
+          <Select
+            multiple
+            value={selectedNames}
+            onChange={(e) => setSelectedNames(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+            renderValue={(selected) => (
+              <Stack gap={1} direction="row" flexWrap="wrap">
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={value}
+                    onDelete={() =>
+                      setSelectedNames(
+                        selectedNames.filter((item) => item !== value)
+                      )
+                    }
+                    deleteIcon={
+                      <CancelIcon
+                        onMouseDown={(event) => event.stopPropagation()}
+                      />
+                    }
+                  />
+                ))}
+              </Stack>
+            )}
+          >
+            {names.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <div>
           <Button
             variant="contained"
