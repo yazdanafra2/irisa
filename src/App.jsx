83d98971abdf8870396e50508a444e87502d32f4
yazdanafra2 <afrayazdan9@gmail.com>
2024-07-13 16:22:51 +0330

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import MyForm from "./components/MyApp";
-import AddDynamicInputFields from "./components/testMenu";
 
 const App = () => {
   return (
     <>
       <MyForm />
-      {/* <AddDynamicInputFields></AddDynamicInputFields> */}
     </>
   );
 };
